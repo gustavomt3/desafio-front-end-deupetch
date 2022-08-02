b3ef907b8128609765394e31e2396145281e676f
Gustavo Melo <contato@pepinosolution.com>
2022-08-02 13:37:45 -0300

    style: retirado o bootstrap e o styled component. adicionado o sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import GlobalStyle from './styles/global';
 import Home from './pages/Home/index';
 
 function App() {
   return (
     <>
-    <GlobalStyle />
-    <Home />
+      <Home />
     </>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,35 +1,15 @@
-/**
- * @desc [Componente do Header]
- */
-import React, {useState} from 'react';
-import { Container, BoxAside } from './styles';
-
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
 
 //components
 import Search from '../Search';
 import ProductCart from '../ProductCart';
 
 export default function Header() {
-    const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false);
 
-    return(
-        <>
-        <Container>
-            <p>Logo</p>
-            <Search />
-            <Button variant="primary" onClick={() => setActive(true)}>Carrinho (1)</Button>
-        </Container>
-        <BoxAside active={active}>
-            
-            <Button variant="danger" style={{height: 35}} onClick={() => setActive(false)}>X</Button>
-            
-            <div>
-                <p className="text-center">Carrinho</p>
-                <ProductCart props/>
-            </div>
-            <p>Valor total: 170.00</p>
-        </BoxAside>
-        </>
-    );
+  return (
+    <>
+      <div>Header</div>
+    </>
+  );
 }
